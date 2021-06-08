4b8b801e5de808315232520b55b153f85b49fd8e
freire-marcos <marcostavares.dev@gmail.com>
2021-06-08 13:59:23 -0300

    agora com foto de gatinho

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 const IndexPage = () => {
@@ -7,6 +8,10 @@ const IndexPage = () => {
     <Layout pageTitle ='Home page'>
       <h1>Teste de título qualquer</h1>
       <p>testando a primeira page</p>
+      <StaticImage
+        alt='Gato bonitinho'
+        src='https://images.unsplash.com/photo-1495360010541-f48722b34f7d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80'
+        />
     </Layout>
   )
 }
