ab9f0b66d0b79411442d69ec787d11fc72351169
freire-marcos <marcostavares.dev@gmail.com>
2021-06-08 13:38:53 -0300

    structuring

---
+++ src/pages/about.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle='About me'>
+
+            <h1>Motivações dessa página</h1>
+            <p>Simples página para testar as rotas</p>
+
+        </Layout>
+    )
+}
+
+export default AboutPage;
